21166fd7079618ba3e309cb7c74efbd0bb7fe0b3
aaron weiner <aaronjacobweiner@yahoo.com>
2021-04-09 21:10:09 -0400

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Notes
+# Note Taker  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Description
+This project was created to give the ability to a person to write notes and save them. It was also created to help give me a better understanding of how to interact with front end files and create the server to handle all the requests the front end is making.
+
--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const fs = require("fs");
 const util = require("util");
 const app = express();
 const PORT = 8080;
-const asyncReadFile = util.promisify(fs.readFile);
+const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const { v4: uuidv4 } = require('uuid');
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,42 +15,38 @@ app.use(express.static('public'));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-
 app.get("/api/notes", (req, res) => {
-    asyncReadFile("./db/db.json", "utf8").then((result) => {
+    readFile("./db/db.json", "utf8").then((result) => {
         const parsedNotes = JSON.parse(result)
         res.json(parsedNotes)
     });
 })
 
 app.post('/api/notes', (req, res) => {
+    console.log(req);
     const newNote = req.body;
     newNote.id = uuidv4();
     console.log(newNote);
-    asyncReadFile("./db/db.json", "utf8").then((result) => {
+    readFile("./db/db.json", "utf8").then((result) => {
         const parsedNotes = JSON.parse(result)
         parsedNotes.push(newNote);
         writeFile("./db/db.json", JSON.stringify(parsedNotes, null, "\t")).then(() => {
             res.json(parsedNotes);
         })
     });
-
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-
     const chosen = req.params.id;
     console.log(res);
     console.log(chosen);
-
-    asyncReadFile("./db/db.json", "utf8").then((result) => {
+    readFile("./db/db.json", "utf8").then((result) => {
         const parsedNotes = JSON.parse(result)
         const filteredNotes = parsedNotes.filter(val => val.id !== chosen);
         writeFile("./db/db.json", JSON.stringify(filteredNotes, null, "\t")).then(() => {
             res.json(filteredNotes);
         })
     });
-
 });
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
